1fbeb54806ebbefde37d29b8f81256a19245726a
krot1941 <yaroslav.sedov203@gmail.com>
2022-04-05 01:43:14 +0300

    DEl one more warning

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -12,10 +12,7 @@ public abstract class Player {
     /**
      * Finds monster tied to player
      * @return the monster 
-     */
-	 public Player() {
-	this.monster = NULL;
-	}	
+     */	
      public Player(Monster monster) {
        this.monster = monster;
     }
