18a3d640e682f96c80ec8839d3333f7ef5847028
krot1941 <yaroslav.sedov203@gmail.com>
2022-04-05 00:02:43 +0300

    Ebaniy suppres

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -74,6 +74,7 @@ public abstract class Player {
 
             //Determines if attack is super or not very effective and determines modifier
             //multiplyed by 2 b/c using int later divided by 2
+	@SuppressWarnings({"UseEqualsToCompareStrings","CompareObjectsWithEquals"})
             for(int i=0; i <= 2; i++) {
                 if (this.monster.getMove(m).getType() == weakness[i]) { 
                     isSuperEffective = 4;
